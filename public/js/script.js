4de3c444f37843b6709f1271a5f29eef274ba40f
Anjali Aggarwal <anjaliaggarwal1703@gmail.com>
2026-03-06 15:46:27 +0530

    Improved marker UI and hover tooltip for user IDs in real-time tracker

--- public/js/script.js
+++ public/js/script.js
@@ -3,6 +3,37 @@ const socket = io();
 let userCount = 0;
 const userCountElement = document.getElementById("userCount");
 
+const redIcon = new L.Icon({
+    iconUrl: "https://raw.githubusercontent.com/pointhi/leaflet-color-markers/master/img/marker-icon-red.png",
+    shadowUrl: "https://unpkg.com/leaflet@1.9.3/dist/images/marker-shadow.png",
+    iconSize: [35, 55],
+    iconAnchor: [17, 55]
+});
+
+const blueIcon = new L.Icon({
+    iconUrl: "https://raw.githubusercontent.com/pointhi/leaflet-color-markers/master/img/marker-icon-blue.png",
+    shadowUrl: "https://unpkg.com/leaflet@1.9.3/dist/images/marker-shadow.png",
+    iconSize: [35, 55],
+    iconAnchor: [17, 55]
+});
+
+const greenIcon = new L.Icon({
+    iconUrl: "https://raw.githubusercontent.com/pointhi/leaflet-color-markers/master/img/marker-icon-green.png",
+    shadowUrl: "https://unpkg.com/leaflet@1.9.3/dist/images/marker-shadow.png",
+    iconSize: [35, 55],
+    iconAnchor: [17, 55]
+});
+
+const userIcons = {};
+
+function getUserIcon(id) {
+    if (!userIcons[id]) {
+        const icons = [redIcon, blueIcon, greenIcon];
+        userIcons[id] = icons[Math.floor(Math.random() * icons.length)];
+    }
+    return userIcons[id];
+}
+
 const markers = {};
 
 if (navigator.geolocation) {
@@ -34,7 +65,7 @@ if (navigator.geolocation) {
 
 }
 
-const map = L.map("map").setView([0,0], 18);
+const map = L.map("map").setView([0, 0], 20);
 
 L.tileLayer("https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png", {
     attribution: "© OpenStreetMap contributors"
@@ -45,18 +76,30 @@ socket.on("receive-location", (data) => {
 
     const { id, latitude, longitude } = data;
 
-    map.setView([latitude, longitude]);
+    if (id === socket.id) {
+        map.setView([latitude, longitude], 18);
+    }
 
     if (markers[id]) {
 
         markers[id].setLatLng([latitude, longitude]);
 
-    } 
+    }
     else {
 
-        markers[id] = L.marker([latitude, longitude])
+        const offsetLat = latitude + (Math.random() - 0.5) * 0.0001;
+        const offsetLng = longitude + (Math.random() - 0.5) * 0.0001;
+
+        markers[id] = L.marker([offsetLat, offsetLng], {
+            icon: getUserIcon(id)
+        })
             .addTo(map)
-            .bindPopup(`User: ${id.substring(0,6)}`);
+            .bindTooltip(`User ${id.substring(0, 4)}`, {
+                direction: "top",
+                offset: [0, -10],
+                className: "user-label",
+                opacity: 0.9
+            });
 
         userCount++;
         userCountElement.textContent = userCount;
